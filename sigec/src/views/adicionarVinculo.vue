9932c2a83a2b4adaabe01d67a9bdd2ef87d0b55e
tads98 <thales.a@academico.ifrn.edu.br>
2021-10-04 21:44:46 -0300

    corrigindo exibição de transparencia, incluindo soma e exibicao corrigida dos blocos

--- sigec/src/views/adicionarVinculo.vue
+++ sigec/src/views/adicionarVinculo.vue
@@ -1,51 +1,44 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="750"
-    style="margin-top: 5%"
-  >
+  <v-card class="mx-auto" max-width="750" style="margin-top: 5%">
     <v-card-title id="position">Adicionar um novo vínculo</v-card-title>
 
     <v-form @submit.prevent="addVinculo">
-        <v-text-field 
-          v-model="perfil"
-          label="Cargo" 
-          name="cargo" 
-          type="text" 
-          class="rounded-pill" 
-          outlined
-        >
-        </v-text-field>
-        <v-text-field 
-          label="Estabelecimento de Saúde" 
-          name="estabelecimento" 
-          type="text" 
-          class="rounded-pill" 
-          outlined
-        >
-        </v-text-field>
-
-    <v-card-actions id="button-add">
-      <v-btn
-        type="submit" 
-        class="rounded-pill mb-5 btn-lg" 
-        color="indigo darken-4" 
-        dark
+      <v-text-field
+        v-model="perfil"
+        label="Cargo"
+        name="cargo"
+        type="text"
+        class="rounded-pill"
+        outlined
+        dense
       >
-      <v-icon> mdi-plus-circle-outline</v-icon>
-        Adicionar Vínculo
-      </v-btn>
-
-      <!-- <v-btn
-        type="submit" 
-        class="rounded-pill mb-5 btn-lg" 
-        color="red darken-1"
-        dark
+      </v-text-field>
+      <v-text-field
+        label="Estabelecimento de Saúde"
+        name="estabelecimento"
+        type="text"
+        class="rounded-pill"
+        outlined
+        dense
       >
-      <v-icon> mdi-close-circle-outline</v-icon>
-        Cancelar
-      </v-btn> -->
-    </v-card-actions>
+      </v-text-field>
+
+      <v-card-actions id="button-add">
+        <v-btn
+          type="submit"
+          class="rounded-pill mb-5 btn-lg"
+          color="indigo darken-4"
+          dark
+        >
+          <v-icon> mdi-plus-circle-outline</v-icon>
+          Adicionar Vínculo
+        </v-btn>
+
+        <v-btn class="rounded-pill mb-5 btn-lg" color="red darken-1" dark>
+          <v-icon> mdi-close-circle-outline</v-icon>
+          Cancelar
+        </v-btn>
+      </v-card-actions>
     </v-form>
   </v-card>
 </template>
@@ -55,47 +48,60 @@ import axios from "axios";
 
 export default {
   name: "adicionarVinculo",
-  data(){
-    return{
+  
+  data() {
+    return {
       perfil: "",
-      estabelecimento: "", 
+      estabelecimento: "",
     };
   },
 
-  methods: {
-    addVinculo(){
-      axios({
-        method: "post",
-        url: "/vinculos",
-        data: {
-          perfil: this.perfil,
-          estabelecimento: this.estabelecimento,
-        },
-      })
-      .then((response) => {
-        this.perfil = "";
-        this.estabelecimento = "";
+  // methods: {
+  //   addVinculo() {
+  //     axios({
+  //       method: "post",
+  //       url: "vinculos",
+  //       data: {
+  //         perfil: this.perfil,
+  //         estabelecimento: this.estabelecimento,
+  //       },
+  //     })
+  //       .then((response) => {
+  //         this.perfil = "";
+  //         this.estabelecimento = "";
 
-        console.log(response);
-        this.$router.push("/dashboard");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    },
-  },
+  //         console.log(response);
+  //         this.$router.push("/dashboard");
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //       });
+  //   },
+  // },
+
+  methods:{
+    addVinculo(){
+      const formData = {
+        perfil: this.perfil,
+        estabelecimento: this.estabelecimento
+      }
+      axios.post('vinculo', formData)
+      .then(response => console.log(response))
+      .catch(error => (console.log(error)))
+    }
+  }
 };
 </script>
 
 <style scoped>
-    #position, .v-form{
-        max-width: 400px;
-        margin-left: 5%;
-        color: indigo;
-    }
-    
-    #button-add{
-        margin-left: 5%;
-    }
-    
+#position,
+.v-form {
+  max-width: 400px;
+  margin-left: 5%;
+  color: indigo;
+}
+
+#button-add {
+  margin-left: 5%;
+}
 </style>
--- sigec/src/views/transparencia.vue
+++ sigec/src/views/transparencia.vue
@@ -1,115 +1,132 @@
 <template>
-<v-main>
-  <v-card
-    class="mx-auto"
-    max-width="800"
-    style="margin-top: -4%; height: 110%;"
-  >
-    <v-card-text
-        red--text
-    >
-      <h1 style="color: orange;" class="text-center">
-        Transparência
-      </h1>
-    </v-card-text>
-    <div class="container">
-        
-        <div class="row">
-            <div class="col-sm-4">
+  <div class="sheet">
+    <v-container class="mt-5">
+    <h1>Transparência</h1>
+        <v-row>
+            <v-col>
+                <v-row class="ma-0">
+                    <v-card width="100%" color="indigo darken-4" dark>
+                        <v-card-title class="justify-end"
+                            ><v-icon>mdi-clipboard-text</v-icon
+                            >{{ indicadores.tratados }}</v-card-title
+                        >
+                        <v-card-text class="text-right"
+                            ><p>Casos tratados</p></v-card-text
+                        >
+                    </v-card>
+                </v-row>
+                <v-row class="ma-0 mt-2">
+                    <v-card width="100%" style="border: 1px solid blue">
+                    <v-card-title class="blue--text justify-end"
+                        ><v-icon color="blue darken-2">mdi-clipboard-text</v-icon
+                        >{{ indicadores.suspeitos }}</v-card-title
+                    >
+                    <v-card-text class="blue--text text-end"
+                        ><p>Casos suspeitos</p></v-card-text
+                    >
+                    </v-card>
+                </v-row>
+            </v-col>
+            <v-col>
+                <v-row class="ma-0">
+                    <v-card width="100%" style="border: 1px solid green">
+                        <v-card-title class="green--text justify-end"
+                            ><v-icon color="green darken-2" id="teste"
+                            >mdi-clipboard-text</v-icon
+                            >{{ indicadores.descartados }}</v-card-title
+                        >
+                        <v-card-text class="green--text text-end"
+                            ><p>Casos descartados</p></v-card-text
+                        >
+                    </v-card>
+                </v-row>
+                <v-row class="ma-0 mt-2">
+                    <v-card width="100%" style="border: 1px solid blue">
+                        <v-card-title class="blue--text justify-end"
+                            ><v-icon color="blue darken-2">mdi-clipboard-text</v-icon
+                            >{{ indicadores.perdidos }}</v-card-title
+                        >
+                        <v-card-text class="blue--text text-end"
+                            ><p>Casos perdidos</p></v-card-text
+                        >
+                    </v-card>
+                </v-row>
+            </v-col>
+            <v-col>
                 <v-card
-                    color="indigo darken-4"
-                    dark
-                >
-                    <v-card-title class="justify-end"><v-icon>mdi-clipboard-text</v-icon>{{ indicadores.tratados }}</v-card-title>
-                    <v-card-text class="text-right"><p>Casos tratados</p></v-card-text>
-                </v-card>
-            </div>
-            <div class="col-sm-4">
-                <v-card
-                    style="border: 1px solid green;"
-                >
-                    <v-card-title class="green--text justify-end"><v-icon color="green darken-2" id="teste">mdi-clipboard-text</v-icon>295.074</v-card-title>
-                    <v-card-text class="green--text text-end"><p>{{ indicadores.descartados }}</p></v-card-text>
-                </v-card>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-4">
-                <v-card
-                    style="border: 1px solid blue;"
-                >
+                        style="border: 1px solid #006699;"
+                    >
+                    
+                        <v-card-title class="blue--text justify-end">{{ sum }}</v-card-title>
+                        <v-card-text class="blue--text text-end"><p>Casos de covid</p></v-card-text>
+                        <v-card-text class="blue--text text-left"><p>jovens(0-19 anos) {{ indicadores.estratificacao.jovens }}</p></v-card-text>
+                        <v-card-text class="blue--text text-left"><p>Adultos(20 - 59 anos) {{ indicadores.estratificacao.adultos }}</p></v-card-text>
+                        <v-card-text class="blue--text text-left"><p>Idosos(acima de 60 anos) {{ indicadores.estratificacao.idosos }}</p></v-card-text>
                 
-                    <v-card-title class="blue--text justify-end"><v-icon  color="blue darken-2">mdi-clipboard-text</v-icon>{{ indicadores.suspeitos }}</v-card-title>
-                    <v-card-text  class="blue--text text-end"><p>Casos suspeitos</p></v-card-text>
-               
-                </v-card>
-            </div>
-            <div class="col-sm-4">
-                <v-card
-                    style="border: 1px solid orange;"
-                >
-                
-                    <v-card-title class="orange--text justify-end"><v-icon color="orange darken-2">mdi-clipboard-text</v-icon>{{ indicadores.perdidos }}</v-card-title>
-                    <v-card-text class="orange--text text-end"><p>Casos perdidos</p></v-card-text>
-               
-                </v-card>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-8 h-75">
-                <v-card
-                    style="border: 1px solid #006699;"
-                >
-                
-                    <v-card-title class="blue--text justify-end">359.847</v-card-title>
-                    <v-card-text class="blue--text text-end"><p>Casos de covid</p></v-card-text>
-                    <v-card-text class="blue--text text-left"><p>jovens(0-19 anos) {{ indicadores.estratificacao.jovens }}</p></v-card-text>
-                    <v-card-text class="blue--text text-left"><p>Adultos(20 - 59 anos) {{ indicadores.estratificacao.adultos }}</p></v-card-text>
-                    <v-card-text class="blue--text text-left"><p>Idosos(acima de 60 anos) {{ indicadores.estratificacao.idosos }}</p></v-card-text>
-               
-                </v-card>
-            </div>
-        </div>
-    </div>
-  </v-card>
-</v-main>
+                    </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-    name: "transparencia",
+  name: "transparencia",
 
-    data(){
-        return {
-            indicadores: [],
-        };
+  data() {
+    return {
+      indicadores: [],
+    };
+  },
+
+  mounted() {
+    this.getIndicadores();
+  },
+
+  methods: {
+    getIndicadores() {
+      axios.get("/indicadores").then((response) => {
+        this.indicadores = response.data.data;
+        var estr = this.indicadores.estratificacao;
+        this.sum = estr.jovens + estr.adultos + estr.idosos;   
+      });
     },
-
-    mounted() {
-        this.getIndicadores();
-    },
-
-    methods: {
-        getIndicadores(){
-            axios.get("/indicadores").then((response) => {
-                this.indicadores = response.data.data;
-            })
-        }
-    }
-}
+  },
+};
 </script>
 
 <style lang="css" scoped>
-    main  {
-        background-image: url("../assets/background.png");
-    }
-    .v-icon{
-        margin-right: 60%;
-    }
+.sheet {
+  height: 100vh;
+  overflow: hidden;
+  background-image: url("../assets/background.png");
+  background-position: top;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 
-    #teste{
-        margin-right: 50%;
-    }
+h1 {
+  font-weight: inherit;
+  text-align: center;
+  color: #fca311;
+  margin-bottom: 2em;
+}
+
+.container{
+  width: 64%;
+  background-color: white;
+}
+/* main {
+  background-image: url("../assets/background.png");
+} */
+.v-icon {
+  margin-right: 60%;
+}
+
+#teste {
+  margin-right: 50%;
+}
 </style>

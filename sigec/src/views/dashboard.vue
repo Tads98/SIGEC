e9bbf8be4450064c1740d48581b55e1f4a013636
tads98 <thales.a@academico.ifrn.edu.br>
2021-10-06 10:45:22 -0300

    Corrigindo disposicao de cards em listagem de dashboard

--- sigec/src/views/dashboard.vue
+++ sigec/src/views/dashboard.vue
@@ -1,48 +1,48 @@
-<template>
-  
+/* <template>
   <div class="center">
-      <div>
-        <h1>Olá, Kelly Gomez</h1>
-        <p class="float-right">Adicione um vínculo <v-icon> mdi-plus-circle-outline</v-icon></p>
-        <p>Selecione um vínculo</p>
-        <v-divider></v-divider>
-      </div>
-      <div v-for="vinculo in vinculos" :key="vinculo.id">
-        <v-card
-          class="mt-5"
-          max-width="344"
-          outlined
-        >
-        
-          <v-list-item three-line>
-            <v-list-item-content>
-              <v-list-item-title class="text-h5 mb-1">
-                <v-icon>
-                  mdi mdi-hospital
-                </v-icon>
-                {{ vinculo.perfil }}
-              </v-list-item-title>
-              <v-list-item-subtitle>
-                <v-icon>mdi-map-marker</v-icon>
-                <p>{{ vinculo.estabelecimento }}</p>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-
-          <v-card-actions>
-            <v-btn
-              name="submit-button"
-              type="submit"
-              dark
-              color="blue"
-              block
-              class="rounded-pill"
-              ><strong>Acessar</strong></v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </div>
+    <div>
+      <h1>Olá, Kelly Gomez</h1>
+      <p class="float-right">
+        Adicione um vínculo <v-icon> mdi-plus-circle-outline</v-icon>
+      </p>
+      <p>Selecione um vínculo</p>
+      <v-divider></v-divider>
     </div>
+    <v-row>
+      <v-col>
+        <v-row>
+          <div v-for="vinculo in vinculos" :key="vinculo.id">
+            <v-card class="mt-5 mr-5" max-width="344" outlined>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="text-h5 mb-1">
+                    <v-icon> mdi mdi-hospital </v-icon>
+                    {{ vinculo.perfil }}
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                    <v-icon>mdi-map-marker</v-icon>
+                    <p>{{ vinculo.estabelecimento }}</p>
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-card-actions>
+                <v-btn
+                  name="submit-button"
+                  type="submit"
+                  dark
+                  color="blue"
+                  block
+                  class="rounded-pill"
+                  ><strong>Acessar</strong></v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </div>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -52,24 +52,23 @@ import axios from "axios";
 export default {
   name: "dashboard",
 
-   data() {
-     return {
-       vinculos: [],
-     };
-   },
+  data() {
+    return {
+      vinculos: [],
+    };
+  },
 
-   mounted() {
-     this.getVinculos();
-   },
-  
+  mounted() {
+    this.getVinculos();
+  },
+
   methods: {
     getVinculos() {
       axios.get("/vinculos").then((response) => {
         this.vinculos = response.data.data;
-      })
-    }
+      });
+    },
   },
-
 
   // data() {
   //   return {
@@ -98,3 +97,4 @@ export default {
   padding: 10px;
 }
 </style>
+ */
